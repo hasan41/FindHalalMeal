de002464fe66ef4f34b4943107d75693f0124477
hasan41 <hasan.naseer1470@gmail.com>
2024-01-19 01:52:44 -0500

    Code refactoring

---
+++ assests/constants.js
@@ -0,0 +1,45 @@
+export const ICONS = [
+    { id: 0, name: 'food', label: 'Food' },
+    { id: 1, name: 'hamburger', label: 'Burger' },
+    { id: 2, name: 'french-fries', label: 'Fries' },
+    { id: 3, name: 'pizza', label: 'Pizza' },
+    { id: 4, name: 'coffee', label: 'Coffee' },
+    { id: 5, name: 'ice-cream', label: 'Dessert' },
+    { id: 6, name: 'fruit-watermelon', label: 'Shakes' },
+    { id: 7, name: 'food-steak', label: 'Steak' },
+    // Add more icons here
+  ];
+
+  export const EXPLORE_ICONS = [
+    { name: 'pizza', label: 'Pizza' },
+    { name: 'hamburger', label: 'Burger' },
+    { name: 'food-halal', label: 'Halal' },
+    { name: 'cake', label: 'Cake' },
+    { name: 'cookie', label: 'Cookie' },
+    { name: 'ice-cream', label: 'Ice Cream' },
+    { name: 'coffee', label: 'Coffee' },
+    { name: 'beer', label: 'Beer' },
+    { name: 'food-apple', label: 'Apple' },
+    { name: 'food-turkey', label: 'Turkey' },
+    { name: 'food-kosher', label: 'Kosher' },
+    { name: 'food-steak', label: 'Steak' },
+  ];
+  
+  export const EXPLORE_RECTANGLE_ICONS = [
+    { name: 'hamburger', label: 'Burgers' },
+    { name: 'french-fries', label: 'Fries' },
+    { name: 'food-variant', label: 'Dairy' },
+    { name: 'food', label: 'Fast Food' },
+    { name: 'food-fork-drink', label: 'Dinner' },
+    { name: 'food-steak', label: 'Steak' },
+    { name: 'food-croissant', label: 'Sweets' },
+    { name: 'fruit-cherries', label: 'Fruits' },
+    { name: 'ice-cream', label: 'IceCream' },
+    { name: 'food-turkey', label: 'Turkish' },
+    { name: 'food-takeout-box', label: 'Takeout' },
+    { name: 'fruit-watermelon', label: 'Shakes' },
+  ];
+  
+  
+  export const PERMISSION_DENIED_ERROR = 'Permission to access location was denied';
+  
